9b3ab7f06edae573400fb02bb369a59f376ff722
BryanGarciaR <bgarcia1989@gmail.com>
2025-03-15 19:21:02 -0500

    Tu mensaje de commit

---
+++ src/main/java/EC3_GARCIALOPEZ/EC3_GARCIALOPEZ/Ec3GarcialopezApplication.java
@@ -0,0 +1,13 @@
+package EC3_GARCIALOPEZ.EC3_GARCIALOPEZ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec3GarcialopezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec3GarcialopezApplication.class, args);
+	}
+
+}
---
+++ src/test/java/EC3_GARCIALOPEZ/EC3_GARCIALOPEZ/Ec3GarcialopezApplicationTests.java
@@ -0,0 +1,13 @@
+package EC3_GARCIALOPEZ.EC3_GARCIALOPEZ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec3GarcialopezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
