2c82071ae1c6c9818d6c82772a4e351adcf3509e
BryanGarciaR <bgarcia1989@gmail.com>
2025-03-16 10:44:17 -0500

    avanze

--- src/main/java/EC3_GARCIALOPEZ/EC3_GARCIALOPEZ/model/Libro.java
+++ src/main/java/EC3_GARCIALOPEZ/EC3_GARCIALOPEZ/model/Libro.java
@@ -1,4 +1,37 @@
 package EC3_GARCIALOPEZ.EC3_GARCIALOPEZ.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Libro {
+    @Id
+    private Integer id;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    private String titulo;
+    private String autor;
 }
